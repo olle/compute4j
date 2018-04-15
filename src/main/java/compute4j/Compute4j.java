84b9645f4e3add8981f0703d7f3e47f3cd24538f
Olle Törnström <toernstroem@synyx.de>
2018-04-15 09:52:17 +0200

    Initial commit

---
+++ src/main/java/compute4j/Compute4j.java
@@ -0,0 +1,111 @@
+package compute4j;
+
+import compute4j.logging.Loggable;
+
+import org.apache.commons.codec.binary.Hex;
+
+import org.apache.logging.log4j.util.ProcessIdUtil;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+import java.util.Locale;
+
+
+@SpringBootApplication
+public class Compute4j {
+
+    public static void main(String[] args) throws Exception {
+
+        new SpringApplicationBuilder(Compute4j.class).web(WebApplicationType.NONE).run(args);
+
+        // TODO: Remove when listeners are active...
+        // Thread.currentThread().join();
+    }
+}
+
+@EnableRabbit
+@Configuration
+class RabbitMqConfig implements Loggable {
+
+    private static final String COMPUTE4J = "compute4j";
+
+    @Bean
+    Exchange createCompute4jExchange() {
+
+        return log_created(ExchangeBuilder.fanoutExchange(COMPUTE4J).autoDelete().build());
+    }
+
+
+    @Bean(name = "compute4jQueue")
+    public Queue createCompute4jQueue() {
+
+        return log_created(QueueBuilder.nonDurable(queueName()).autoDelete().build());
+    }
+
+
+    private String queueName() {
+
+        return String.format(Locale.ENGLISH, "%s.%s.%s", COMPUTE4J, clientAddress(), processId());
+    }
+
+
+    private String clientAddress() {
+
+        String result = "unknown";
+
+        try {
+            InetAddress address = InetAddress.getLocalHost();
+            result = address.toString();
+
+            NetworkInterface network = NetworkInterface.getByInetAddress(address);
+            byte[] mac = network.getHardwareAddress();
+
+            return Hex.encodeHexString(mac);
+        } catch (UnknownHostException | SocketException e) {
+            return result;
+        }
+    }
+
+
+    private String processId() {
+
+        return ProcessIdUtil.getProcessId();
+    }
+
+
+    @Bean
+    Binding createCompute4jBinding() {
+
+        return log_created(BindingBuilder.bind(createCompute4jQueue())
+                .to(createCompute4jExchange())
+                .with("")
+                .noargs());
+    }
+
+
+    @RabbitListener(queues = "#{@compute4jQueue}")
+    public void subscribe(Message message) {
+
+        logger().info("||--> Received {}", message);
+    }
+}
---
+++ src/main/java/compute4j/logging/Loggable.java
@@ -0,0 +1,25 @@
+package compute4j.logging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * Logging trait.
+ */
+public interface Loggable {
+
+
+    default Logger logger() {
+
+        return LoggerFactory.getLogger(this.getClass());
+    }
+
+
+    default <T> T log_created(T obj) {
+
+        logger().info("// Created {}", obj);
+
+        return obj;
+    }
+}
